7fc5e2f7d0519f98fa74fedded02481c681ef102
mahdi <mahdimamashli1383@gmail.com>
2024-07-13 20:33:11 +0330

    feat: "Removed MY_TOKEN and BASE_URL from telegram.js, added BASE_URL to config.js"

--- src/api/telegram.js
+++
@@ -1,2 +0,0 @@
-const MY_TOKEN = process.env.BOT_TOKEN
-const BASE_URL = `https://api.telegram.org/bot${MY_TOKEN}/`
--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,5 @@ module.export = {
   telegramToken: process.env.TELEGRAM_BOT_TOKEN,
   webhookUrl: process.env.WEBHOOK_URL,
   port: process.env.PORT || 3000,
+  BASE_URL : process.env.BASE_URL
 };
