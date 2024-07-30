04894407f646090c23fadcef3b0362df13b3905c
mahdi <mahdimamashli1383@gmail.com>
2024-07-31 02:58:11 +0330

    feat(app): imported new modules, uncommented middleware and commands, and wrapped bot.start() in an async function with database initialization and error handling.

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import { Bot } from 'grammy';
 import {Command } from './controller/command';
-// import { handleWarnCommand, /* handleMuteCommand, handleBanCommand*/ } from './controller/manager';
 // import { welcomeNewMember } from './api/welcome';
 // import { automatedResponses } from './api/responses';
 // import { logActivity } from './api/logging';
 // import { addCustomCommand, executeCustomCommand } from './api/customCommands';
-// import { checkAdmin } from './middleware/isAdmin';
+import { checkAdmin } from './middleware/isAdmin';
+import { ManagementGroup } from './controller/manager';
+import { AppDataSource } from './config/db';
+import { logger } from './helper/logging';
 
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
-bot.command('start', Command.start);
-bot.command('help', Command.help);
-bot.command('admins', Command.admins);
+// bot.command('start', Command.start);
+// bot.command('help', Command.help);
+// bot.command('admins', Command.admins);
 
-// bot.command('warn', checkAdmin, handleWarnCommand);
+bot.command('warn', checkAdmin, ManagementGroup.warn);
 // bot.command('mute', checkAdmin, handleMuteCommand);
 // bot.command('ban', checkAdmin, handleBanCommand);
 // bot.command('addcommand', checkAdmin, addCustomCommand);
@@ -23,4 +25,13 @@ bot.command('admins', Command.admins);
 // bot.on('message', logActivity);
 // bot.on('message', executeCustomCommand);
 
-bot.start();
+(async () => {
+    try {
+        await AppDataSource.initialize();
+        logger.info('DATABASE INITIALIZED','DATABASE')
+        bot.start();
+        logger.info("BOT STARTED","APP");
+    } catch (error:any) {
+        logger.error('ERROR FROM START APP',error,'APP')
+    }
+})();
