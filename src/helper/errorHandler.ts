3ac6c4baddc80d235a22f676bd1fb05f96cd60cf
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:51:00 +0330

    feat(helper): improve error handling in the `handleError` function
    - Added detailed logging for errors, including command, userId, chatId, and stackTrace.
    - Enhanced context information for better debugging and issue tracking.

---
+++ src/helper/errorHandler.ts
@@ -0,0 +1,23 @@
+import { Context } from "grammy";
+import { logger } from "./logging";
+export async function handleError(
+  ctx: Context,
+  error: any,
+  propertyKey: string
+) {
+  const userId = ctx.from?.id || "Unknown";
+  const chatId = ctx.chat?.id || "Unknown";
+  const command = ctx.message?.text?.split(" ")[0] || "Unknown command";
+  // Log the error with detailed context
+  logger.error(
+    `An error occurred in ${propertyKey}:`,
+    error,
+    "Context",
+    {
+      command,
+      userId,
+      chatId,
+      stackTrace: error.stack,
+    }
+  );
+}
