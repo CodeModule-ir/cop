b642762d049ac783a13a2e587bcb28da7380f202
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:43:15 +0330

    refactor(controller): "Refactor Command class: add SafeExecution decorator, new methods, and modify existing ones"

--- src/controller/command.ts
+++ src/controller/command.ts
@@ -1,14 +1,50 @@
-import { Context } from 'grammy';
-import { startCommand, helpCommand } from '../service/command';
-import { listAdmins } from '../admin-tools/roles';
-export class Command{
-  static async start(ctx:Context){
-    await startCommand(ctx)
+import { Context } from "grammy";
+import { MESSAGE } from "../helper/message";
+import { SafeExecution } from "../decorators/SafeExecution";
+import { listAdmins } from "../user-managemen/roles";
+import { AdminCommand } from "../user-managemen/AdminCommand";
+import { UserManagement } from "../user-managemen/UserManagement";
+export class Command {
+  @SafeExecution()
+  static start(ctx: Context) {
+    if (ctx.chat?.type === "private") {
+      return ctx.reply(MESSAGE.START());
+    }
+    return ctx.reply(MESSAGE.START(), {
+      reply_parameters: {
+        message_id: ctx.message?.message_id!,
+      },
+    });
   }
-  static async help(ctx:Context){
-    await helpCommand(ctx)
+  @SafeExecution()
+  static help(ctx: Context) {
+    if (ctx.chat?.type === "private") {
+      return ctx.reply(MESSAGE.HELP());
+    }
+    return ctx.reply(MESSAGE.HELP(), {
+      parse_mode: "Markdown",
+      reply_parameters: {
+        message_id: ctx.message?.message_id!,
+      },
+    });
   }
-  static async admins(ctx:Context){
-    await listAdmins(ctx)
+  @SafeExecution()
+  static async admins(ctx: Context) {
+    await listAdmins(ctx);
+  }
+
+  @SafeExecution()
+  static async warn(ctx: Context): Promise<void> {
+    await AdminCommand.Warn(ctx);
+  }
+
+  @SafeExecution()
+  static async clear(ctx: Context): Promise<void> {
+    await AdminCommand.WarnClear(ctx);
+  }
+
+  @SafeExecution()
+  static async me(ctx: Context): Promise<void> {
+    await UserManagement.Info(ctx);
   }
 }
