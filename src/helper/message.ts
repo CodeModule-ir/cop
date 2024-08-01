593d65bec54ce7ffaf887a90a49974b17e903c80
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:55:26 +0330

    feat(helper): add static methods for user warnings and info management
    - Added MESSAGE.WARN() for warning users.
    - Added MESSAGE.WARNING_CLEARED() for clearing warnings.
    - Added MESSAGE.NO_WARNINGS() for when no warnings are present.
    - Added MESSAGE.WARN_CLEAR() for successful warning clearance.
    - Added MESSAGE.ME() for user information retrieval.
    - Updated MESSAGE.HELP() and MESSAGE.START() for improved command descriptions.

---
+++ src/helper/message.ts
@@ -0,0 +1,43 @@
+import { User } from "grammy/types";
+import { RepliedMessage } from "../types/types";
+
+export class MESSAGE {
+  static USERINFO() {}
+  static WARN(msg: RepliedMessage) {
+    return `@${msg.from?.username} (${msg.from?.first_name}) has been warned. They now have 1 warning.\nFor a detailed list of rules, use the command: /rules`;
+  }
+  static WARNING_CLEARED(user: string, usernameAdmin: string) {
+    return `Warnings for @${user} have been cleared by @${usernameAdmin}. Please review the group rules with /rules. Thank you for contributing to a positive community!`;
+  }
+  static NO_WARNINGS() {
+    return "This user has no warnings.";
+  }
+  static WARN_CLEAR() {
+    return `The warnings for the user have been successfully cleared.`;
+  }
+  static ME(user: User, isAdmin: boolean, count: number) {
+    return `<b>User Information:</b>\n
+<strong>ID:</strong> ${user.id}\n
+<strong>Name:</strong> ${user.first_name} ${
+      user.last_name ? user.last_name : ""
+    }\n
+<strong>Username:</strong> @${user.username ?? "N/A"}\n
+<strong>Status:</strong> ${isAdmin ? "Admin" : "Member"}\n
+<strong>Number of Warnings:</strong> ${count ?? 0}\n`;
+  }
+  static HELP() {
+    return `
+Welcome to **CMCOP** Here are the commands you can use:
+  - /start: Start interacting with the bot and receive a welcome message.
+  - /help: Get assistance and see the list of available commands.
+  - /warn: Issue a warning to a user.
+  - /mute [time]: Mute a user for a specified time.
+  - /ban [user]: Ban a user from the group.
+  - /admins: List all admins in the chat.
+  - /addcommand [command] [response]: Add a custom command.
+`;
+  }
+  static START() {
+    return "Hello! I’m CMCOP, your friendly group management assistant. Use /help to see what I can do!";
+  }
+}
