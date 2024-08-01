4ec8adbeee39a819052e64b4c364e089821df8a8
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:30:04 +0330

    feat(app): "Removed unused imports and commands, refactored bot initialization and logging, and added new GenerateCommand and callbackQuery handlers."

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,20 @@
-import { Bot } from 'grammy';
-import {Command } from './controller/command';
-// import { welcomeNewMember } from './api/welcome';
-// import { automatedResponses } from './api/responses';
-// import { logActivity } from './api/logging';
-// import { addCustomCommand, executeCustomCommand } from './api/customCommands';
-import { checkAdmin } from './middleware/isAdmin';
-import { ManagementGroup } from './controller/manager';
-import { AppDataSource } from './config/db';
-import { logger } from './helper/logging';
-
+import { Bot } from "grammy";
+import { AppDataSource } from "./config/db";
+import { logger } from "./helper/logging";
+import { CmdValidator } from "./middleware";
+import { Button } from "./controller/button";
+import { GenerateCommand } from "./helper/gen.command";
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
-
-// bot.command('start', Command.start);
-// bot.command('help', Command.help);
-// bot.command('admins', Command.admins);
-
-bot.command('warn', checkAdmin, ManagementGroup.warn);
-// bot.command('mute', checkAdmin, handleMuteCommand);
-// bot.command('ban', checkAdmin, handleBanCommand);
-// bot.command('addcommand', checkAdmin, addCustomCommand);
-
-// bot.on('message:new_chat_members', welcomeNewMember);
-// bot.on('message', automatedResponses);
-// bot.on('message', logActivity);
-// bot.on('message', executeCustomCommand);
+new GenerateCommand(bot).generate()
+bot.callbackQuery(/^remove_warning_(\d+)$/, CmdValidator.isAdmin, Button.clear);
 
 (async () => {
-    try {
-        await AppDataSource.initialize();
-        logger.info('DATABASE INITIALIZED','DATABASE')
-        bot.start();
-        logger.info("BOT STARTED","APP");
-    } catch (error:any) {
-        logger.error('ERROR FROM START APP',error,'APP')
-    }
+  try {
+    await AppDataSource.initialize();
+    logger.info("DATABASE INITIALIZED", "DATABASE");
+    bot.start();
+    logger.info("BOT STARTED", "APP");
+  } catch (error: any) {
+    logger.error("ERROR FROM START APP", error, "APP");
+  }
 })();
