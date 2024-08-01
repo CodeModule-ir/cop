fe4b45cd7ec6525d561a2649801d932b5b02130e
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:56:42 +0330

    refactor(middleware):"Refactor isAdmin middleware to use SafeExecution decorator and Middleware class"

--- src/middleware/isAdmin.ts
+++ src/middleware/isAdmin.ts
@@ -1,11 +1,30 @@
-import { Context, NextFunction } from 'grammy';
-import { isUserAdmin } from '../helper/helper';
+import { SafeExecution } from "../decorators/SafeExecution";
+import { logger } from "../helper/logging";
+import { Middleware } from "./mid";
+export class IsAdmin extends Middleware {
+  @SafeExecution()
+  async execute(): Promise<void> {
+    const userId = (await this.bot.getUser()).id;
+    if (!userId) {
+      logger.error("User ID is not available.", undefined, "CheckAdmin", {
+        context: this.ctx.message,
+      });
+      await this.ctx.reply(
+        "Oops! We couldn't determine your identity. Please try again."
+      );
+      return;
+    }
 
-export async function checkAdmin(ctx: Context, next: NextFunction) {
-  const userId = ctx.message?.from?.id;
-  if (userId && (await isUserAdmin(ctx, userId))) {
-    return next();
-  } else {
-    await ctx.reply('You need to be an admin to use this command.');
+    const isAdmin = await this.bot.isUserAdmin(userId);
+    if (isAdmin) {
+      return this.nxt();
+    } else {
+      await this.ctx.reply(
+        "Sorry, but you need to be an admin to use this command!",
+        {
+          reply_to_message_id: this.ctx.message?.message_id,
+        }
+      );
+    }
   }
 }
