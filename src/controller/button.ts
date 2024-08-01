909f9e22873b5e70604406112de2835f0e6eb3cb
mahdi <mahdimamashli1383@gmail.com>
2024-08-01 19:44:04 +0330

    feat(controller): add Button clear warnings functionality
    - Implement `clear` method in `Button` class to handle clearing warnings for a user.
    - Update the message in the chat and provide user feedback upon success or failure.
    - Add feedback through callback query response and updated message text.

---
+++ src/controller/button.ts
@@ -0,0 +1,23 @@
+import { Context } from "grammy";
+import { BotService } from "../helper/manager";
+import { WarnService } from "../service/warning";
+import { MESSAGE } from "../helper/message";
+
+export class Button {
+  /**
+   * Clears warnings for a user and updates the message in the chat.
+   * Handles the callback query to remove warnings and provides user feedback.
+   *
+   * @param ctx - The context of the callback query.
+   */
+  static async clear(ctx: Context) {
+    const { BotManager, InlineKeyboard } = new BotService(ctx);
+    if (await new WarnService(ctx, InlineKeyboard.userId).clear()) {
+      const username = await InlineKeyboard.getWarnUsername();
+      await ctx.answerCallbackQuery("Warnings removed!");
+      await ctx.editMessageText( MESSAGE.WARNING_CLEARED(username,BotManager.user?.username!));
+    } else {
+      await ctx.answerCallbackQuery("No warnings to remove.");
+    }
+  }
+}
