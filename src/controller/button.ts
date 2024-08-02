39a69fffa3510e3a360109933d21599870162689
mahdi <mahdimamashli1383@gmail.com>
2024-08-02 05:07:05 +0330

    feat(controller): Added new command along with handler for messages
    - `command` : Two new methods were created to handle the mute and unmute commands
    
    - `button`:Messages are handled in a separate class

--- src/controller/button.ts
+++ src/controller/button.ts
@@ -1,6 +1,6 @@
 import { Context } from "grammy";
 import { BotService } from "../helper/manager";
-import { WarnService } from "../service/warning";
+import { WarnService } from "../service/warn";
 import { MESSAGE } from "../helper/message";
 
 export class Button {
@@ -15,7 +15,9 @@ export class Button {
     if (await new WarnService(ctx, InlineKeyboard.userId).clear()) {
       const username = await InlineKeyboard.getWarnUsername();
       await ctx.answerCallbackQuery("Warnings removed!");
-      await ctx.editMessageText( MESSAGE.WARNING_CLEARED(username,BotManager.user?.username!));
+      await ctx.editMessageText(
+        MESSAGE.WARNING_CLEARED(username, BotManager.user?.username!)
+      );
     } else {
       await ctx.answerCallbackQuery("No warnings to remove.");
     }
