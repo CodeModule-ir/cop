46c4759197a7047e0ee6e01873d517fa4830e69d
mahdi <mahdimamashli1383@gmail.com>
2024-08-03 21:08:27 +0330

    feat(config) replaced Mute with GroupMembership in entities array

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 import { Warning } from "../entities/Warning";
 import { User } from "../entities/User";
 import { GroupSettings } from "../entities/GroupSettings";
-import { Mute } from "../entities/Mute";
+import { GroupMembership } from "../entities/GroupMembership";
 export const AppDataSource = new DataSource({
   type: "mariadb",
   host: process.env.DB_HOST,
@@ -10,6 +10,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User, Warning,Mute, GroupSettings],
+  entities: [User, Warning, GroupSettings, GroupMembership],
   synchronize: true,
 });
