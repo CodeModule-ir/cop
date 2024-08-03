29e4b567039350583c33ca1130e8e96d9b6803b6
mahdi <mahdimamashli1383@gmail.com>
2024-08-03 21:07:50 +0330

    refactor(controller): removed admin-related methods, added command map, and introduced handleCommand method to process incoming commands

--- src/controller/command.ts
+++ src/controller/command.ts
@@ -1,14 +1,32 @@
 import { Context } from "grammy";
 import { MESSAGE } from "../helper/message";
 import { SafeExecution } from "../decorators/SafeExecution";
-import { listAdmins } from "../user-management/roles";
-import { AdminCommand } from "../user-management/AdminCommand";
-import { UserManagement } from "../user-management/UserManagement";
+import { AdminCommand } from "../group-management/AdminCommand";
 export class Command {
+   private static commandMap: { [key: string]: (ctx: Context) => Promise<any> } =  {
+    "start":Command.start,
+    "help":Command.help,
+    "warn": AdminCommand.Warn,
+    "rmWarn": AdminCommand.WarnClear,
+    "mute": AdminCommand.mute,
+    "unMute": AdminCommand.MuteClear,
+    "ban": AdminCommand.ban,
+    "unBan": AdminCommand.unBan,
+    "purge": AdminCommand.Purge,
+    "approved": AdminCommand.approved,
+    "unApproved": AdminCommand.unApproved,
+    "lock": AdminCommand.lock,
+    "unLock": AdminCommand.unLock,
+    "blacklist": AdminCommand.getBlackList,
+    "abl":AdminCommand.addBlackList,
+    "rmbl":AdminCommand.RemoveMessageBlackList,
+    "date": AdminCommand.date
+  };
+
   @SafeExecution()
   static start(ctx: Context) {
     if (ctx.chat?.type === "private") {
-      return ctx.reply(MESSAGE.START());
+      return ctx.reply(MESSAGE.PV_START());
     }
     return ctx.reply(MESSAGE.START(), {
       reply_parameters: {
@@ -29,32 +47,33 @@ export class Command {
     });
   }
   @SafeExecution()
-  static async admins(ctx: Context) {
-    await listAdmins(ctx);
-  }
+  static async handleCommand(ctx: Context): Promise<void> {
+    /**
+     * Extracts the command from a message that may include additional text after the command.
+     *
+     * For example, if the message is "/warn example", this code will:
+     * 1. Split the message by the '/' character.
+     * 2. Take the second part of the split result (which is "warn example").
+     * 3. Further split this part by spaces and take the first element (which is "warn").
+     *
+     * This ensures that only the command itself is extracted, excluding any subsequent arguments.
+     */
+const command = ctx.message?.text?.split("/")[1]?.split(/[\s@]/)[0];
+  console.log(command);
 
-  @SafeExecution()
-  static async warn(ctx: Context): Promise<void> {
-    await AdminCommand.Warn(ctx);
-  }
-
-  @SafeExecution()
-  static async unWarn(ctx: Context): Promise<void> {
-    await AdminCommand.WarnClear(ctx);
-  }
-
-  @SafeExecution()
-  static async me(ctx: Context): Promise<void> {
-    await UserManagement.Info(ctx);
-  }
-
-  @SafeExecution()
-  static async mute(ctx: Context): Promise<void> {
-    await AdminCommand.mute(ctx);
-  }
-
-  @SafeExecution()
-  static async unMute(ctx: Context): Promise<void> {
-    await AdminCommand.MuteClear(ctx);
+    if (command) {
+      const commandFunction = Command.commandMap[command];
+      if (commandFunction) {
+        await commandFunction(ctx);
+      } else {
+        await ctx.reply("Unknown command.", {
+          reply_to_message_id: ctx.message?.message_id,
+        });
+      }
+    } else {
+      await ctx.reply("Command is missing.", {
+        reply_to_message_id: ctx.message?.message_id,
+      });
+    }
   }
 }
