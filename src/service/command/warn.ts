0441fe9074be2ebc61859774e958cc231ad0414a
mahdi <mahdimamashli1383@gmail.com>
2024-08-10 20:01:44 +0330

    reafactor(serivce/command) :removed logger, added GroupSettingsService, and updated warn method to include group

--- src/service/command/warn.ts
+++ src/service/command/warn.ts
@@ -3,20 +3,20 @@ import { BanService } from "./ban";
 import { MESSAGE } from "../../helper/message";
 import { UserService } from "../db/user";
 import { WarningServiceDb } from "../db/user/warning";
-import { Logger } from "../../config/logger";
 import { SafeExecution } from "../../decorators/SafeExecution";
-const logger = new Logger({file:"warnService.log",level:'debug',timestampFormat:'locale',})
+import { GroupSettingsService } from "../db/group";
 export class WarnService {
   private userId: number;
   private ctx: Context;
   private userRepo: UserService;
   private warnRepo: WarningServiceDb;
-
+  private groupRepo: GroupSettingsService
   constructor(ctx: Context, userId: number) {
     this.userId = userId;
     this.ctx = ctx;
     this.userRepo = new UserService();
     this.warnRepo = new WarningServiceDb();
+    this.groupRepo = new GroupSettingsService()
   }
 
   /**
@@ -24,32 +24,39 @@ export class WarnService {
    */
   // @SafeExecution()
   async warn(reason: string = "unknown") {
-    let user = await this.userRepo.findByRelations(this.userId, "warnings");
+    let user = await this.userRepo.findByRelations(
+      this.userId,
+      "warnings",
+    );
     if (!user) {
-      logger.info('User not found, creating new user', 'WARN_SERVICE', { userId: this.userId });
       user = await this.userRepo.createUser(this.ctx, this.userId);
     }
+    const groupId = this.ctx.chat?.id!;
+
+    // Check if user is already a member of the group, if not, add them
+    const group = await this.groupRepo.getByGroupId(groupId); 
+    if (!group) {
+      await this.groupRepo.init(this.ctx)
+    }
+
     // Create and save the new warning
-    const warning = await this.warnRepo.create({
+    const warning = this.warnRepo.create({
       user,
+      group: group!,
       reason,
     });
-    await this.warnRepo.save(warning);
-    logger.info('Warning added to user', 'WARN_SERVICE', { userId: this.userId, warningId: warning.id });
+    await this.warnRepo.save(warning)
     // Update user warnings list
     user.warnings.push(warning);
     await this.userRepo.save(user);
-    logger.debug('User warnings updated', 'WARN_SERVICE', { userId: this.userId, warningCount: user.warnings.length });
-
     // Check warning count
     const warningCount = await this.warnRepo.count(user);
     if (warningCount >= 3) {
-      logger.warn('User has reached warning limit', 'WARN_SERVICE', { userId: this.userId, warningCount });
       const msg: string = await new BanService(this.ctx, this.userId).ban();
       await this.warnRepo.clear(user.id);
-      logger.info('User banned and warnings cleared', 'WARN_SERVICE', { userId: this.userId, message: msg });
       return { warning, banned: true, message: msg };
     }
+
     return { warning, banned: false, count: warningCount };
   }
 
@@ -58,18 +65,14 @@ export class WarnService {
    */
   @SafeExecution()
   async clear() {
-    logger.debug('Attempting to clear warnings for user', 'WARN_SERVICE', { userId: this.userId });
     const user = await this.userRepo.findByRelations(this.userId, "warnings");
     if (!user) {
-      logger.info('No warnings found for user', 'WARN_SERVICE', { userId: this.userId });
       return MESSAGE.NO_WARNINGS();
     }
 
     await this.warnRepo.clear(user.id);
     user.warnings = [];
     await this.userRepo.save(user);
-    logger.info('All warnings cleared for user', 'WARN_SERVICE', { userId: this.userId });
-
     return MESSAGE.WARN_CLEAR();
   }
 
@@ -78,17 +81,12 @@ export class WarnService {
    */
   @SafeExecution()
   async count(): Promise<number> {
-    logger.debug('Counting warnings for user', 'WARN_SERVICE', { userId: this.userId });
-
     const user = await this.userRepo.findByRelations(this.userId, "warnings");
     if (!user) {
-      logger.info('No user found for warning count', 'WARN_SERVICE', { userId: this.userId });
       return 0;
     }
 
     const warningCount = user.warnings.length;
-    logger.debug('Warning count for user', 'WARN_SERVICE', { userId: this.userId, warningCount });
-
     return warningCount;
   }
 }
