12e48d15fd06a88032fdc460d338167b87af8b0d
mahdi <bitsgenix@gmail.com>
2024-11-13 16:28:43 +0330

    refactor: overhaul project structure and switch to PostgreSQL
    - Reorganized project structure to enhance modularity and maintainability
    - Replaced MySQL with PostgreSQL as the database, switching to the `pg` package for database operations
    - Removed TypeORM and all related dependencies
    - Refactored database relationships to be handled directly via `pg` package instead of ORM
    - Separated bot logic from the database layer for improved code organization and separation of concerns
    - Applied Object-Oriented Programming (OOP) principles to the new structure to ensure better modularity and code reusability

--- src/config/db.ts
+++
@@ -1,6 +0,0 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import { getAppDataSourceConfig } from "./index";
-
-const config: DataSourceOptions = getAppDataSourceConfig() as DataSourceOptions;
-
-export const AppDataSource = new DataSource(config);
