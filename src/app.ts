19aaab31b42ed276ac92adcfb3cf8cb3f7544a5c
mahdi <bitsgenix@gmail.com>
2024-11-14 03:01:37 +0330

    feat: initialize database and bot in main entry point
    - Set up `DatabaseService` to initialize the database connection and seed tables.
    - Added logging for database initialization.
    - Instantiate and initialize the `CopBot` in the main entry point (`app.ts`).
    - Added logging for bot initialization.

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,11 @@
-import { Bot } from "grammy";
-import { GenerateCommand } from "./service/command/generator";
-import { MessageCheck } from "./service/MessageCheck";
-import { Spam } from "./service/bot/spam";
-import { logger } from "./config/logger";
-import { db } from "./service/db";
-const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
-new GenerateCommand(bot).generate();
-bot.on("message", async (ctx) => {
-  if (!ctx.chat) {
-    return;
-  }
-  await MessageCheck.Message(ctx);
-  await MessageCheck.isCode(ctx);
-  await MessageCheck.CheckBlackList(ctx);
-  await Spam.WarnSpam(ctx);
-  await MessageCheck.isNewUser(ctx);
-  await MessageCheck.leftGroup(ctx);
-});
-bot.on("my_chat_member", MessageCheck.initialGroup);
-(async () => {
-  try {
-    await db.initialize();
-    bot.start();
-    logger.info("BOT STARTED", "APP");
-  } catch (error: any) {
-    logger.error("ERROR FROM START APP", error.message, "APP");
-    await db.close();
-    process.exit(1);
-  }
-})();
+import { CopBot } from './bot';
+import { DatabaseService } from './database';
+import logger from './utils/logger';
+async function main() {
+  const cop = new CopBot();
+  await new DatabaseService().initialize();
+  logger.info('initialize Database');
+  await cop.initial();
+  logger.info('initial bot');
+}
+main();
