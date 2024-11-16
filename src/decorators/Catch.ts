ae820672a96b198fa5cf819098934356209b327b
mahdi <bitsgenix@gmail.com>
2024-11-16 16:26:51 +0330

    feat(decorators): implement `Catch` decorator for centralized error handling in async methods
    - Add `Catch` decorator:
      - Handles errors in decorated asynchronous methods.
      - Supports custom error responses via the `ErrorResponse` type.
      - Integrates `grammy` context (`Context`) to send user-friendly error messages in bot interactions.
      - Logs errors with detailed metadata, including the target class, method, and error category.
      - Automatically handles specific errors (e.g., `bot was kicked`) by cleaning up group data via the `GroupService`.
    
    - Utilize `ServiceProvider` to retrieve necessary services dynamically.
    - Improve system reliability by ensuring consistent error handling and user feedback in case of failures.

---
+++ src/decorators/Catch.ts
@@ -0,0 +1,38 @@
+import { Context } from 'grammy';
+import { ErrorResponse } from '../types/ResponseTypes';
+import logger from '../utils/logger';
+import { ServiceProvider } from '../service/database/ServiceProvider';
+export function Catch(customResponse?: ErrorResponse) {
+  return function (target: any, propKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function (...args: any[]) {
+      const ctx: Context = (this as any)?.ctx || args[0];
+      try {
+        return await originalMethod.apply(this, args);
+      } catch (error: any) {
+        if (error.error_code === 403 && error.description.includes('bot was kicked')) {
+          const chatId = error.payload?.chat_id;
+          logger.warn(`[Warning] Bot was kicked from a group (chat_id: ${error.payload.chat_id}). Skipping.`);
+          const service = ServiceProvider.getInstance();
+          const groupService = await service.getGroupService();
+          await groupService.delete(chatId);
+          return;
+        }
+        // Custom response or default error message
+        const errorResponse: ErrorResponse = customResponse || {
+          message: error.message,
+          statusCode: error.statusCode || 500,
+          category: 'General',
+        };
+        logger.error(`[Category: ${errorResponse.category}] Error in ${target.constructor.name}.${propKey}(): ${error.message}`);
+        // Send the error message to the user (if context is available)
+        if (ctx && typeof ctx.reply === 'function') {
+          await ctx.reply(errorResponse.message);
+        }
+        throw error;
+      }
+    };
+
+    return descriptor;
+  };
+}
