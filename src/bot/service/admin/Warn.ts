a89fc315acaf9d6f29525323ae3da9bdb1e4454c
mahdi <bitsgenix@gmail.com>
2024-11-17 05:19:21 +0330

    feat(group-settings): implement welcome message management and warnings system
    - Added functionality to manage group welcome messages:
      - Retrieve (`getWelcomeMessage`), set (`setWelcomeMessage`), and remove (`removeWelcomeMessage`) welcome messages for groups.
    
    - Introduced warnings management system:
      - Warn users (`warnUser`) with reasons and track warnings per user and group.
      - Remove warnings (`removeWarn`) for users.
      - Retrieve warnings count for a specific user (`getUserWarnById`).
      - Generate a detailed group warning report (`getAllWarns`) with structured output.
    
    - Integrated services (`ServiceProvider`) for database interaction:
      - Group, user, and warning services provide core data handling.
    
    - Ensured proper validation using `AdminValidationService` for context validation.

---
+++ src/bot/service/admin/Warn.ts
@@ -0,0 +1,179 @@
+import { Context } from 'grammy';
+import { AdminValidationService } from './validation';
+import { ServiceProvider } from '../../../service/database/ServiceProvider';
+
+export class WarnService {
+  static async warnUser(ctx: Context): Promise<{
+    warningApplied: boolean;
+    isWarningLimitReached: boolean;
+    warnings: number;
+  }> {
+    const validationResult = await AdminValidationService.validateContext(ctx);
+    if (!validationResult) {
+      return {
+        warningApplied: false,
+        isWarningLimitReached: false,
+        warnings: 0,
+      };
+    }
+    const { groupId, userId } = validationResult;
+    const services = ServiceProvider.getInstance();
+    const input = ctx.message?.text!.split(/\s+/).slice(1);
+    const reason = input!.join('')?.toLowerCase() || 'reason is not set for warning';
+    const [groupService, userService, warnService] = await Promise.all([services.getGroupService(), services.getUserService(), services.getWarnsService()]);
+    let group = await groupService.getByGroupId(groupId);
+    let user = await userService.getByTelegramId(userId);
+    const userData = { first_name: ctx!.message?.reply_to_message?.from?.first_name!, id: userId, username: ctx.message?.reply_to_message?.from?.username! };
+    if (!user) {
+      user = await userService.save(userData);
+    }
+    if (!group) {
+      group = await groupService.save(ctx);
+    }
+    let warn = await warnService.getByGroupId(groupId);
+    if (!warn) {
+      warn = await warnService.save(group.id, user.id, reason);
+    }
+
+    const warningsGroup = [...group.warnings.map(Number)];
+    if (!warningsGroup.includes(+user.telegram_id)) {
+      warningsGroup.push(+user.telegram_id);
+    }
+    // Remove the user from the group's approved_users and members arrays
+    const updatedGroup = {
+      ...group,
+      warnings: warningsGroup,
+    };
+    const updatedUser = {
+      ...user,
+      warnings: Number(user.warnings) + 1, // Increment user warnings
+    };
+    const updatedWarning = {
+      ...warn,
+      reason: reason,
+    };
+    if (updatedUser.warnings >= 3) {
+      // Reset the warnings count and clear the user from the group's warning list
+      const updatedGroup = {
+        ...group,
+        warnings: group.warnings.filter((id) => Number(id) !== Number(user.telegram_id)),
+      };
+      const updatedUser = {
+        ...user,
+        warnings: 0,
+      };
+      await warnService.delete(warn.id);
+      await Promise.all([groupService.update(updatedGroup), userService.update(updatedUser)]);
+      return {
+        warningApplied: true,
+        isWarningLimitReached: true,
+        warnings: 0,
+      };
+    }
+    await Promise.all([groupService.update(updatedGroup), userService.update(updatedUser), warnService.update(updatedWarning)]);
+    return {
+      warningApplied: true,
+      isWarningLimitReached: false,
+      warnings: updatedUser.warnings,
+    };
+  }
+  static async removeWarn(ctx: Context): Promise<{ warningRemoved: boolean; warnings: number }> {
+    const validationResult = await AdminValidationService.validateContext(ctx);
+    if (!validationResult) {
+      return { warningRemoved: false, warnings: 0 };
+    }
+
+    const { groupId, userId } = validationResult;
+    const services = ServiceProvider.getInstance();
+    const [groupService, userService] = await Promise.all([services.getGroupService(), services.getUserService()]);
+    let group = await groupService.getByGroupId(groupId);
+    let user = await userService.getByTelegramId(userId);
+    const userData = { first_name: ctx!.message?.reply_to_message?.from?.first_name!, id: userId, username: ctx.message?.reply_to_message?.from?.username! };
+    if (!user) {
+      user = await userService.save(userData);
+    }
+    if (!group) {
+      group = await groupService.save(ctx);
+    }
+    const updatedUser = {
+      ...user,
+      warnings: user.warnings - 1,
+    };
+    await userService.update(updatedUser);
+    return {
+      warningRemoved: true,
+      warnings: updatedUser.warnings,
+    };
+  }
+  static async getUserWarnById(ctx: Context): Promise<{ warnings: number }> {
+    const validationResult = await AdminValidationService.validateContext(ctx);
+    if (!validationResult) {
+      return { warnings: 0 };
+    }
+
+    const { userId } = validationResult;
+    const services = ServiceProvider.getInstance();
+    const userService = await services.getUserService();
+    let user = await userService.getByTelegramId(userId);
+    let replyMessage = ctx.message?.reply_to_message?.from;
+    const userData = { first_name: replyMessage?.first_name!, id: userId, username: replyMessage?.username! };
+    if (!user) {
+      user = await userService.save(userData);
+    }
+
+    return { warnings: user.warnings };
+  }
+  static async getAllWarns(ctx: Context): Promise<string> {
+    const replyMessage = ctx.from;
+    // Ensure the user ID of the replied message is valid
+    const userId = replyMessage!.id!;
+    const chat = ctx.chat;
+    const groupId = chat?.id!;
+    const services = ServiceProvider.getInstance();
+    const [groupService, userService, warnService] = await Promise.all([services.getGroupService(), services.getUserService(), services.getWarnsService()]);
+    let group = await groupService.getByGroupId(groupId);
+    let user = await userService.getByTelegramId(userId);
+    const userData = { first_name: ctx!.message?.reply_to_message?.from?.first_name!, id: userId, username: ctx.message?.reply_to_message?.from?.username! };
+    if (!user) {
+      user = await userService.save(userData);
+    }
+    if (!group) {
+      group = await groupService.save(ctx);
+    }
+    let warn = await warnService.getByGroupId(groupId);
+    if (!warn) {
+      warn = await warnService.save(group.id, user.id, 'The reason for the warning is not specified');
+    }
+    // Get all users from the group's warning list
+    const usersWithWarnings: { first_name: string; userId: string; warnings: number; reason: string }[] = [];
+    for (const userId of group.warnings) {
+      const user = await userService.getByTelegramId(userId);
+      if (user) {
+        const reason = warn.reason;
+        usersWithWarnings.push({
+          first_name: user.first_name,
+          userId: `${user.telegram_id}`,
+          warnings: user.warnings,
+          reason: reason || 'No specific reason provided',
+        });
+      }
+    }
+
+    // Generate structured output
+    let output = `**Group Warning Report**\n`;
+    output += `Group ID: ${groupId}\n\n`;
+    output += `**Users with Warnings:**\n`;
+
+    usersWithWarnings.forEach((user, index) => {
+      output += `\n${index + 1}. Name: ${user.first_name}\n`;
+      output += `   User ID: ${user.userId}\n`;
+      output += `   Warnings: ${user.warnings}\n`;
+      output += `   Reason: ${user.reason}\n`;
+    });
+
+    // Add summary
+    output += `\n---\nTotal Users with Warnings: ${usersWithWarnings.length}\n`;
+
+    return output;
+  }
+}
---
+++ src/bot/service/admin/Welcome.ts
@@ -0,0 +1,56 @@
+import { Context } from 'grammy';
+import { ServiceProvider } from '../../../service/database/ServiceProvider';
+import { Group } from '../../../types/database/TablesTypes';
+export class GroupSettingsService {
+  static async getWelcomeMessage(ctx: Context, welcomeContent: string): Promise<Group['welcome_message']> {
+    const { groupService } = await GroupSettingsService.getServices();
+    const groupId = ctx.chat?.id!;
+    let group = await groupService.getByGroupId(groupId);
+    if (!group) {
+      group = await groupService.save(ctx);
+    }
+    return group.welcome_message;
+  }
+  static async removeWelcomeMessage(ctx: Context): Promise<string> {
+    const { groupService } = await GroupSettingsService.getServices();
+    const groupId = ctx.chat?.id!;
+
+    // Get the group by its ID
+    let group = await groupService.getByGroupId(groupId);
+    if (!group) {
+      // If no group exists, create a new one
+      group = await groupService.save(ctx);
+    }
+
+    // Remove the welcome message (set it to an empty string or null)
+    const updatedGroup = await groupService.update({
+      ...group,
+      welcome_message: '',
+    });
+
+    return updatedGroup.welcome_message ? updatedGroup.welcome_message : 'The welcome message has been removed.';
+  }
+  static async setWelcomeMessage(ctx: Context, welcomeContent: string): Promise<string> {
+    const { groupService } = await GroupSettingsService.getServices();
+    const groupId = ctx.chat?.id!;
+    // Get the group by its ID
+    let group = await groupService.getByGroupId(groupId);
+    if (!group) {
+      // If no group exists, create a new one
+      group = await groupService.save(ctx);
+    }
+    // Update the welcome message
+    const updatedGroup = await groupService.update({
+      ...group,
+      welcome_message: welcomeContent,
+    });
+    return updatedGroup.welcome_message;
+  }
+  private static async getServices() {
+    const service = ServiceProvider.getInstance();
+    const groupService = await service.getGroupService();
+    return {
+      groupService,
+    };
+  }
+}
