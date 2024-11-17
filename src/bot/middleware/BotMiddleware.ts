ad2103044cc7cb12d4aa3673dbb703546de24bcf
mahdi <bitsgenix@gmail.com>
2024-11-17 05:21:13 +0330

    feat(middleware): add comprehensive admin and chat validation middleware
    Introduced a `BotMiddleware` class with several middleware functions to validate bot commands based on chat type and user permissions.
    
    - Added Features:
      - `userIsAdmin`: Validates if the user executing the command is an admin. Returns an error message if not.
      - `botIsAdmin`: Checks if the bot has admin privileges in the group chat and responds with an error message if insufficient permissions.
      - `isGroupChat`: Ensures the command is used in a valid group chat type (e.g., `supergroup`, `channel`) and restricts execution in private chats.
      - `adminCheckForRepliedUser`: Validates admin status of the replied user and blocks commands like `grant`, `revoke`, and others on admins or the bot itself.
    
    - Improved Usability:
      - Middleware functions can be used both inline (`nxt`) and standalone (`isMiddleware` flag).
      - Detailed error messages provide clarity to the users about restrictions.
    
    - Logging:
      - Added a debug log for commands handled in `adminCheckForRepliedUser`.
    
    This update enhances the bot's security and ensures appropriate command execution in group environments.

---
+++ src/bot/middleware/BotMiddleware.ts
@@ -0,0 +1,90 @@
+import { Context, NextFunction } from 'grammy';
+import { BotReply } from '../../utils/chat/BotReply';
+import { BotInfo } from '../../utils/chat/BotInfo';
+import { ChatInfo } from '../../utils/chat/ChatInfo';
+
+export class BotMiddleware {
+  static async userIsAdmin(ctx: Context, nxt: NextFunction | null, isMiddleware: boolean = true) {
+    const reply = new BotReply(ctx);
+    const chatInfo = new ChatInfo(ctx);
+    const userIsAdmin = await chatInfo.userIsAdmin();
+    if (!userIsAdmin) {
+      await reply.textReply('Sorry, but you need to be an admin to use this command!');
+      return false;
+    }
+    if (isMiddleware) {
+      return nxt!();
+    } else {
+      return true;
+    }
+  }
+  static async botIsAdmin(ctx: Context, nxt: NextFunction | null, isMiddleware: boolean = true) {
+    const reply = new BotReply(ctx);
+    const bot = new BotInfo(ctx);
+    const botIsAdmin = await bot.isAdmin();
+    const message = `
+Sorry, but I don't have the necessary permissions to perform this action. Please ensure that I am an admin in this group.
+    `;
+    if (!botIsAdmin) {
+      await reply.textReply(message);
+      return false;
+    }
+    if (isMiddleware) {
+      return nxt!();
+    } else {
+      return true;
+    }
+  }
+  static async isGroupChat(ctx: Context, nxt: NextFunction | null, isMiddleware: boolean = true) {
+    const chat = ctx.chat;
+    const reply = new BotReply(ctx);
+    if (chat) {
+      if (chat.type === 'supergroup' || chat.type === 'channel') {
+        if (isMiddleware) {
+          return nxt!();
+        } else {
+          return true;
+        }
+      } else if (chat.type === 'private') {
+        await reply.textReply('This command can only be used in group chats');
+        return false;
+      } else {
+        await reply.textReply('This command is not allowed in this type of chat.');
+        return false;
+      }
+    }
+  }
+  /**
+   * Middleware to validate command execution based on admin status.
+   */
+  static async adminCheckForRepliedUser(ctx: Context, nxt: NextFunction | null, isMiddleware: boolean = true) {
+    const reply = new BotReply(ctx);
+    const repliedUserId = ctx.message?.reply_to_message?.from?.id!;
+    const admins = await ctx.api.getChatAdministrators(ctx.chat!.id);
+    const isAdmin = admins!.some((admin) => admin.user.id === repliedUserId);
+    const command = ctx.message?.text?.split('/')[1].split(' ')[0].toLowerCase().trim();
+    console.log('Command:adminCheckForRepliedUser', command);
+    if (command === 'revoke' || command === 'grant' || command === 'pin' || command === 'unpin' || command === 'purge') {
+      if (isMiddleware) {
+        return nxt!();
+      } else {
+        return true;
+      }
+    }
+    if (ctx.me!.id === repliedUserId) {
+      await reply.textReply(`Why should I ${command} myself?`);
+      return false;
+    }
+
+    if (isAdmin) {
+      await reply.textReply(`Why should I ${command} an admin?`);
+      return false;
+    }
+
+    if (isMiddleware) {
+      return nxt!();
+    } else {
+      return true;
+    }
+  }
+}
